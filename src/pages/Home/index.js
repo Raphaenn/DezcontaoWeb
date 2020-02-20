84957d5ed852f5aea5a52a20f7dba8073c8a097d
Raphael Neves <raphaelneves@MacBook-Air-de-Raphael.local>
2020-02-20 17:33:56 -0300

    home page top

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import { Container, Title } from './styles';
+import { Container, Title, Top, First } from './styles';
 import Grafic from "../../components/Grafic";
 import Table from "../../components/Table";
 
@@ -9,7 +9,15 @@ export default function Home() {
 
   return (
     <Container>
-      <Title>Companies Data</Title>
+      <Top>
+        <Title>Dashboard</Title>
+      </Top>
+      <First>
+        <div>Campo 1 - Qtd. de empresas parceiras</div>
+        <div>Campo 2 - N de usuário ativos</div>
+        <div>Campo 3 - Qtd. de tickets utilizados</div>
+        <div>Campo 4 - Notivifações</div>
+      </First>
       <Table />
     </Container>
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,15 +1,50 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    display: flex;
     margin-top: 20px;
+`;
 
+
+export const Top = styled.div`
+    display: block;
+    border-bottom: 1px solid rgba(242, 244, 250, 0.5);
+    padding-top: 1.5rem;
+    padding-bottom: 1.5rem;
+    padding-right: 12px;
+    padding-left: 12px;
+    width: 100%;
+    max-width: 95%;
+    margin-right: auto;
+    margin-left: auto;
 `;
 
 export const Title = styled.strong`
-        font-size: 20px;
-        color: #fff;
-        font-weight: bold;
-        position: absolute;
-        margin: 10px 0 40px 40px;
+    font-size: 2em;
+    color: #fff;
+    font-weight: bold;
+    letter-spacing: -.02em;    
+`;
+
+export const First = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    padding-top: 1.5rem;
+    
+    padding-right: 12px;
+    padding-left: 12px;
+    width: 100%;
+    max-width: 95%;
+    margin: 10px auto 10px auto;
+    align-items: center;
+    justify-content: space-between;
+
+    div {
+        display: flex;
+        width: 300px;
+        height: 100px;
+        background: rgba(242, 244, 250, 0.5);
+        margin-bottom: 1.5rem;
+        border-radius: 4px;
+        align-items: center;
+    }
 `;
